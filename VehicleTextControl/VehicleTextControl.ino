d7e765e0ae7e88a3d4137c909555e9d1885f4993
Alex Pfadenhauer <pfaden@gmail.com>
2017-01-24 20:33:34 -0500

    Debugging & cleaning up code
    SMS send & receive now works correctly. Compilation now completes with
    no warnings or errors. Formatting cleaned up.

--- VehicleTextControl/VehicleTextControl.ino
+++ VehicleTextControl/VehicleTextControl.ino
@@ -1,5 +1,6 @@
 /******************************************************************************* 
- Vehicle SMS Controller 
+
+ - - Vehicle SMS Controller v0.1 - -
 
  Author:      Alex Pfadenhauer
               (pfaden@gmail.com)
@@ -28,35 +29,32 @@
 #include <SoftwareSerial.h>         // used by the shield to communicate with the cellular module
 #include <SFE_MG2639_CellShield.h>  // include the Sparkfun cellular shield library
 
-#define DEBUG       0               // defines the debug variable
+#define DEBUG       1               // defines the debug variable
 
-static char MYCELL[] = "15555555555";         // sets my cell phone number to a constant char array
-static int  LTIME = 100;                      // defines time (in ms) to send the lock signal
-static int  UTIME = 100;                      // defines time (in ms) to send the unlock signal
-static int  STIME = 100;                      // defines time (in ms) to send the signal to start the engine
-static int  DTIME = 100;                      // defines time (in ms) to delay in between sending signals
-char        LastPhoneNum[] = "15555555555";   // creates a char array that holds the last phone number a command was recieved from
-boolean     BrokenPanic = 0;                  // turned on when something goes wrong and the board can't function
-int         messageIndex = 0;
-byte        unreadMsg = 0;
+static char MYCELL[12] = "17034835484";         // sets my cell phone number to a constant char array
+static int  LTIME = 100;                        // defines time (in ms) to send the lock signal
+static int  UTIME = 100;                        // defines time (in ms) to send the unlock signal
+static int  STIME = 100;                        // defines time (in ms) to send the signal to start the engine
+static int  DTIME = 100;                        // defines time (in ms) to delay in between sending signals
+char        LastPhoneNum[12] = "15555555555";   // creates a char array that holds the last phone number a command was recieved from
+boolean     BrokenPanic = 0;                    // turned on when something goes wrong and the board can't function
+int         messageIndex;
+byte        unreadMsg;
 
 void setup() {    // runs once to set up before looping begins
-    if(DEBUG){
-        Serial.begin(9600);             // for verification with laptop
+    if(DEBUG){    // for verification with computer
+        Serial.begin(9600);             
         Serial.println(F("Type anything to begin..."));
-        while(!Serial.available()){ ; }
+        while(!( Serial.available() )){ ; }  // waits until the board and computer are ready to communicate
         Serial.read();
-        Serial.println();
-        Serial.println();
         Serial.println(F("Redboard booting up...."));
     }
     int cellbegin = cell.begin();   // starts up the cell module
-    if(DEBUG){
-        Serial.println(F("Cellular shield booting up...."));
-    }
+    if(DEBUG){ Serial.println(F("Cellular shield starting up....")); }
     delay(5000);                   // give the cell module time to start up
     if (!(1 == cellbegin)){
        BrokenPanic = 1;
+       if(DEBUG){ Serial.println(F("Cellular shield unable to start")); }
     }
     cell.getPhoneNumber(LastPhoneNum);
     if(DEBUG){
@@ -64,98 +62,89 @@ void setup() {    // runs once to set up before looping begins
         Serial.println(LastPhoneNum);
     }
     sms.setMode(SMS_TEXT_MODE);     // sets the text mode to ASCII
-    if(DEBUG){
-        Serial.println(F("Board & shield booted."));
-    }
-    sendText(MYCELL, "Testing this damn thing");
+    if(DEBUG){ Serial.println(F("Board & shield started.")); }
 }
 
 void loop() {      // loops continually while connected to power
     
     delay(10000);   //delays for 10 seconds at the beginning of each loop so it isn't constantly checking
+    unreadMsg = 1;  //sets it up so the index checking loop will start during each loop of the main program
     
     if( 1 == BrokenPanic ){
-        // turn LEDs on or something to panic
-        if(1 == DEBUG){
-        Serial.println(F("BrokenPanic is set! Something is fucked up!"));
-        }
+        // turn on the buzzer, once hardware is ready
+        if(DEBUG){ Serial.println(F("BrokenPanic is set! Something is fucked up!")); }
     }
     else{
-
-
-
-    for (messageIndex = 1; messageIndex < 257; messageIndex++){  // if unreadMsg has not been set yet
-        if(DEBUG){  Serial.println(messageIndex);  }
-        if(0 < sms.read(messageIndex)){
-            unreadMsg = 1;
-            messageIndex = 257;
-            break;
-        }
-    }
- 
-    if( (1 == unreadMsg) ) {                // if there is a message
-        if(DEBUG){
-            Serial.println(F("There are unread messages!"));
-        }
-        strcpy(LastPhoneNum, sms.getSender());      // we update the number from which the last message was recieved
-        String messageContents = sms.getMessage();
-        if (!(isNumberApproved())) {                // the number that texted the last command isn't on the list!
-            if(DEBUG){
-                Serial.println(F("Last message from an unapproved number! The message is:"));
-                Serial.println(messageContents);
-                Serial.println(F("Note: denied number notification text is not sent when debug mode is on"));
-            }
-            else{
-            sendText(MYCELL, String(LastPhoneNum) + (F(" just contacted the Vehicle SMS Controller with the following text:")));
-            sendText(MYCELL, messageContents);
-            }
-        }
-        else {  // the number is on the list! Now, let's perform the command
-            if(DEBUG){
-                Serial.print(F("Test output: messageContents[0] = "));
-                Serial.println(messageContents[0]);
-            }
-            if ('H' == messageContents[0]) {
-                sendText(MYCELL, (F("Available commands are:   HELP, RUN, STOP, LOCK, UNLOCK")));
-            }
-            else if ('R' == messageContents[0]) {   // Start the engine
-                startEngine();
-            }
-            else if ('S' == messageContents[0]) {   // Stop the engine
-                stopEngine();
-            }
-            else if ('U' == messageContents[0]) {   // Unlock the vehicle
-                unlockVehicle();
-            }
-            else if ('L' == messageContents[0]) {   // Lock the vehicle
-                lockVehicle();
-            }
-            else{     // The message text did not begin with one of the above letters.
-                if(DEBUG){
-                    Serial.println(F("command not understood!"));  
-                }
-                else{   
-                    sendText(MYCELL, (F("Last command not understood. Text 'HELP' for a list of available commands")));
+        while(unreadMsg > 0){   // checks indices if we have already found and dealt with one message, or haven't checked yet this loop
+            for (messageIndex = 1; messageIndex < 257; messageIndex++){  // checks each index
+                if(DEBUG){  Serial.println(messageIndex);  } // prints debug messages (each index as it runs through)
+                if(0 < sms.read(messageIndex)){  // if sms.read returns a positive value, there is something at that index
+                    unreadMsg = 2;  // 2 means a message has been found
+                    break;  // stops checking through each index
                 }
             }
-        }
-        sms.deleteMessage(messageIndex);     // delete the message so it doesn't clutter up the SIM mailbox!
-     }  // if (0 != unread messages)
-     else{
+            if(1 == unreadMsg){      // if we rolled through all the indices and there were no messages
+                unreadMsg = 0;      // sets it to 0 to say that no messages have been found, so don't try to check again or to read one 
+            }
+            else if(2 == unreadMsg) {                // if there is a message
+                if(DEBUG){ Serial.println(("There is an unread message at index ") + String(messageIndex)); }
+                strcpy(LastPhoneNum, sms.getSender());      // we update the number from which the last message was recieved
+                String messageContents = sms.getMessage();
+                if (!(isNumberApproved())) {                // the number that texted the last command isn't on the list!
+                    if(DEBUG){
+                        Serial.println(F("Last message from an unapproved number!"));
+                        Serial.println("The number is: " + String(LastPhoneNum));
+                        Serial.println(" My number is: " + String(MYCELL));
+                        Serial.print(F("The message is:"));
+                        Serial.println(messageContents);
+                        Serial.println(F("Note: denied number notification text is not sent when debug mode is on"));
+                        Serial.println("");
+                    }
+                    else{
+                    sendText(MYCELL, String(LastPhoneNum) + (F(" just contacted the Vehicle SMS Controller with the following text:")));
+                    sendText(MYCELL, messageContents);
+                    }
+                }
+                else {  // the number is on the list! Now, let's perform the command
+                    if(DEBUG){
+                        Serial.print(F("messageContents[0] = "));
+                        Serial.println(messageContents[0]);
+                    }
+                    if ('H' == messageContents[0]) {
+                        if(DEBUG){ Serial.println(F("Available commands are:   HELP, RUN, STOP, LOCK, UNLOCK")); }
+                        else{ sendText(MYCELL, (F("Available commands are:   HELP, RUN, STOP, LOCK, UNLOCK"))); }
+                    }
+                    else if ('R' == messageContents[0]) {   // Start the engine
+                        startEngine();
+                    }
+                    else if ('S' == messageContents[0]) {   // Stop the engine
+                        stopEngine();
+                    }
+                    else if ('U' == messageContents[0]) {   // Unlock the vehicle
+                        unlockVehicle();
+                    }
+                    else if ('L' == messageContents[0]) {   // Lock the vehicle
+                        lockVehicle();
+                    }
+                    else{     // The message text did not begin with one of the above letters.
+                        if(DEBUG){ Serial.println(F("command not understood!")); }
+                        else{ sendText(MYCELL, (F("Last command not understood. Text 'HELP' for a list of available commands"))); }
+                    }
+                }
+                if(DEBUG){Serial.println(("Now deleting the last message at index:" + String(messageIndex)));}
+                sms.deleteMessage(messageIndex);     // delete the message so it doesn't clutter up the SIM mailbox!
+                unreadMsg = 1;  // tells the program we are done with this message, but we need to make sure there aren't more left
+            }  // end of code that reads the message
+        } // loop to check for unread messages
         if(DEBUG){  Serial.println(F("no unread messages"));  }
         delay(1000);
-     } // no unread messages
-   } // if cell board didn't start
-
+        } // code that handles no unread messages
 } // loop()
 
 
 
 void sendText(char theRecipient[12], String theMessage){
-    if(DEBUG){
-        Serial.print(("sending text:"));
-        Serial.println(theMessage);
-    }
+    if(DEBUG){ Serial.println("sending text:" + String(theMessage)); }
     sms.start(theRecipient);   // start sending the message, and specify the recipient phone number
     sms.print(theMessage);     // write the body of the message using the text passed to the function
     int smssend = sms.send();  // sends the message and returns a flag (pos for send success, neg for send failure)
@@ -164,50 +153,42 @@ void sendText(char theRecipient[12], String theMessage){
 }
 
 void sendLock(int duration) {
-    if(DEBUG){
-        Serial.println(F("Now Locking"));
-    }
+    if(DEBUG){ Serial.println(F("Now Locking")); }
+    delay(duration);
   return;
 }
 
 
 void sendUnlock(int duration) {
-    if(DEBUG){
-        Serial.println(F("Now Unlocking"));
-    }
+    if(DEBUG){ Serial.println(F("Now Unlocking")); }
+    delay(duration);
   return;
 }
 
 
 boolean isNumberApproved(){
-    if(DEBUG){
-        Serial.print(F("Last rec. num is:"));
-        Serial.println(LastPhoneNum);
-    }
-    if ( MYCELL == LastPhoneNum ) {
-        if(DEBUG){
-            Serial.println(F("The number was on the list!"));
-        }
+    if(DEBUG){ Serial.print("Last rec. num is:" + String(LastPhoneNum)); }
+    if (0 == (strcmp(MYCELL,LastPhoneNum))) {  // check and see if the last phone number is mine
+        if(DEBUG){ Serial.println(F("The number was on the list!")); }
         return true;
     }
-    // if we got this far, then the last text was from a number not on the list!
-    if(DEBUG){
-         Serial.println(F("The number was NOT on the list!"));
-            }
+    if(DEBUG){ Serial.println(F("The number was NOT on the list!")); }
     return false;
 }
 
 
 void unlockVehicle() {
     sendLock(UTIME);     // sends the unlock signal to open the vehicle
-    sendText(MYCELL, (F("The vehicle is now unlocked.")));
+    if(DEBUG){ Serial.println(F("The vehicle has been unlocked")); }
+    else{ sendText(LastPhoneNum, (F("The vehicle is now unlocked."))); }
     return;
 }
 
 
 void lockVehicle() {
     sendLock(LTIME);     // sends the lock signal to lock the vehicle
-    sendText(MYCELL, (F("The vehicle is now locked.")));
+    if(DEBUG){ Serial.println(F("The vehicle has been locked")); }
+    else{ sendText(LastPhoneNum, (F("The vehicle is now locked."))); }
     return;
 }
 
@@ -219,16 +200,18 @@ void startEngine() {     // to start engine, lock twice within 2 seconds, then h
     delay(DTIME);        // pause to make sure commands sent by remote are distinct
     sendLock(STIME);     // sends the lock signal to start the vehicle
     // if hardware functionality is added to check vehicle power (which comes on only when engine is on), add checking for that here
-    sendText(MYCELL, (F("The engine is now started.")));   // send a confirmation text
+    if(DEBUG){ Serial.println(F("The vehicle has been turned on")); }
+    else{ sendText(LastPhoneNum, (F("The vehicle has been turned on."))); }
     return;
 }
 
 
 void stopEngine() {       // to stop the engine, one simply has to unlock the vehicle
-    sendUnlock(UTIME);
+    sendUnlock(UTIME);    // unlock the car to turn the engine off
     delay(DTIME);         // pause to make sure commands sent by remote are distinct
     sendLock(LTIME);      // lock the doors again to make sure we're all secure!
-    sendText(MYCELL, (F("The engine is now stopped.")));   // send a confirmation text
+    if(DEBUG){ Serial.println(F("The vehicle has been turned off")); }
+    else{ sendText(LastPhoneNum, (F("The vehicle has been turned off."))); }
     return;
 }
 
